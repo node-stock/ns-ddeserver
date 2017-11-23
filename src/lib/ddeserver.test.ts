49e64fa37b1e3d52e3f28bbcb7eb8d247b3e78c9
zlq4863947 <zlq4863947@gmail.com>
2017-11-24 03:31:21 +0900

    增加influxdb

--- src/lib/ddeserver.test.ts
+++ src/lib/ddeserver.test.ts
@@ -1,25 +1,10 @@
 import { DdeServer } from './ddeserver';
+import { DdeStream } from './ddestream';
 import { RssStock } from './types';
 import * as assert from 'power-assert';
 import { Log } from 'ns-common';
-import { InfluxDB, FieldType } from 'influx';
 
 const config = require('config');
-const influx = new InfluxDB({
-  host: 'localhost',
-  database: 'test',
-  schema: [
-    {
-      measurement: 'tick',
-      fields: {
-        price: FieldType.INTEGER
-      },
-      tags: [
-        'symbol'
-      ]
-    }
-  ]
-});
 
 Log.init(Log.category.system, Log.level.ALL, 'ns-ddeserver');
 
@@ -46,48 +31,17 @@ const testStart = async (done: any) => {
   done();
 };
 
-const testInflux = async (done: any) => {
+const testDdeStream = async () => {
 
-  /*influx.writePoints([{
-    measurement: 'tick',
-    tags: { symbol: '6553' },
-    fields: { price: 2000 }
-  }]).then(res => console.log(influx.query('select * from tick').then(res2 => {
-    console.log(res2)
-  })));*/
-  /*influx.queryRaw('select * from tick').then(res2 => {
-    console.log('res2:', JSON.stringify(res2, null, 2))
-  })*/
-  // influx.dropContinuousQuery('cq_5m')
-  await influx.createContinuousQuery('cq_5m', `
-    SELECT
-    max(price) as high,
-    min(price) as low,
-    first(price) as open,
-    last(price) as close
-    INTO candlstick_5m
-    FROM tick
-    GROUP BY time(5m), pair
-  `.replace(/\n/g, ''), 'test');
-
-  const res = await influx.showContinousQueries();
-  const cq = res.find(o => o.name === 'cq_5m');
-  console.log('cq:', JSON.stringify(cq, null, 2))
-
-  // TODO
-  // '3667.T'.split('.')[0] = symbol
-  // 判断CQ是否存在，不存在则创建
-  done();
+  const ddeStream = new DdeStream();
+  // 启动Dde服务
+  ddeStream.start().catch((err) => Log.system.error('启动Dde服务出错：', err.stack));
 };
 
 describe('DDE服务测试', () => {
-  it('测试是否启动成功', function (done) {
-    this.timeout(20000);
+  it('测试是否启动成功', function () {
     // testStart(done); // done();
-    done();
-  });
-  it('测试Influx', function (done) {
-    this.timeout(20000);
-    testInflux(done); // done();
+    //return testDdeStream();
+    return;
   });
 });
